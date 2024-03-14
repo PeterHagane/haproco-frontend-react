8b49f3f6043f13d0be247a96b132b7c6f75bed0f
Peter Hagane <peterhagane@yahoo.no>
2024-03-14 04:50:26 +0100

    rename

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => {
   const plugins = [react(), TanStackRouterVite()]
 
   const environmentVars = {
-    'process.env.HELP_HELP': JSON.stringify(env.HELP_HELP),
     'process.env.APP_IS_DEV': JSON.stringify(env.APP_IS_DEV),
     'process.env.API_KEY': JSON.stringify(env.API_KEY),
     'process.env.API_PASSWORD': JSON.stringify(env.API_PASSWORD)
