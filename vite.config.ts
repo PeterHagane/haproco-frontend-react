e56162904593a0fb1b3f9fc389bb9af8f1308fce
Peter Hagane <peterhagane@yahoo.no>
2024-09-07 16:10:11 +0200

    port fix

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,14 @@ export default defineConfig(({ mode }) => {
     'process.env.API_PASSWORD': JSON.stringify(env.API_PASSWORD)
   }
 
-  const port =  process.env.APP_IS_DEV === "true" ? 5174 : 5173;
+  const port =  env.APP_IS_DEV === "true" ? 5174 : 5173;
 
   return {
     define: environmentVars,
     plugins: plugins,
-    port: port
+    server: {
+      port: port
+    }
     }
   }
 )
